647377f7a9538a5c9ded92ef35d5de04cf0e6501
Hrant Davtyan <hr.davtyan@gmail.com>
2017-03-13 15:29:10 +0400

    Final community version

---
+++ PDNA_Dashboard/ui.R
@@ -0,0 +1,9 @@
+library(shinydashboard)
+library(shinyBS)
+library(shinyjs)
+
+source('header.R',local=TRUE)
+source('sidebar.R',local=TRUE)
+source('body.R',local=TRUE)
+
+dashboardPage(header, sidebar, body)
