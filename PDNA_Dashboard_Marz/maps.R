f36b93b5026bafd7816b17bb9366c2aef69bc67a
Hrant Davtyan <hr.davtyan@gmail.com>
2017-03-14 22:22:25 +0400

    added AMD

--- PDNA_Dashboard_Marz/maps.R
+++ PDNA_Dashboard_Marz/maps.R
@@ -11,8 +11,8 @@ output$mymap <- renderLeaflet({
     #addProviderTiles("Stamen.TonerLite",options = providerTileOptions(noWrap = TRUE)) %>%
     addProviderTiles("CartoDB.Positron") %>%
     addMarkers(lat = com_lats, lng = com_lngs,
-               popup=paste0("Total Damage: ",as.character(map_database[["SUM(damages)"]]),br(),
-                            "Total Loss: ",as.character(map_database[["SUM(loss)"]])),
+               popup=paste0("Total Damage: AMD ",as.character(map_database[["SUM(damages)"]]),br(),
+                            "Total Loss: AMD ",as.character(map_database[["SUM(loss)"]])),
                clusterOptions = markerClusterOptions()) %>%
     setView(lat = 40.175576,lng = 44.513343, zoom = 5)
 })
